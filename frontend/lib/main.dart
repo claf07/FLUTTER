1f5c99cf840acf4583872040d5dc30b68cf7877d
Claflin <clafclaflin@example.com>
2025-04-22 11:05:38 +0530

    Cleaned repo and removed sensitive files

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../pages/home.dart';
 import './product_provider.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
